41ea489a47c498d63dedd9c8f86c8c3ad3132bb8
Pavel Janecka <pavel.janecka@eli-beams.eu>
2017-12-06 11:29:07 +0100

    Day 6 in clojure

---
+++ src/advent_of_code/day6.clj
@@ -0,0 +1,65 @@
+; In this area, there are sixteen memory banks; each memory bank can hold any number of blocks. The goal of the reallocation routine is to balance the blocks between the memory banks.
+; The reallocation routine operates in cycles. In each cycle, it finds the memory bank with the most blocks (ties won by the lowest-numbered memory bank) and redistributes those blocks among the banks. To do this, it removes all of the blocks from the selected bank, then moves to the next (by index) memory bank and inserts one of the blocks. It continues doing this until it runs out of blocks; if it reaches the last memory bank, it wraps around to the first one.
+; The debugger would like to know how many redistributions can be done before a blocks-in-banks configuration is produced that has been seen before.
+; For example, imagine a scenario with only four memory banks:
+; The banks start with 0, 2, 7, and 0 blocks. The third bank has the most blocks, so it is chosen for redistribution.
+; Starting with the next bank (the fourth bank) and then continuing to the first bank, the second bank, and so on, the 7 blocks are spread out over the memory banks. The fourth, first, and second banks get two blocks each, and the third bank gets one back. The final result looks like this: 2 4 1 2.
+; Next, the second bank is chosen because it contains the most blocks (four). Because there are four memory banks, each gets one block. The result is: 3 1 2 3.
+; Now, there is a tie between the first and fourth memory banks, both of which have three blocks. The first bank wins the tie, and its three blocks are distributed evenly over the other three banks, leaving it with none: 0 2 3 4.
+; The fourth bank is chosen, and its four blocks are distributed such that each of the four banks receives one: 1 3 4 1.
+; The third bank is chosen, and the same thing happens: 2 4 1 2.
+; At this point, we've reached a state we've seen before: 2 4 1 2 was already seen. The infinite loop is detected after the fifth block redistribution cycle, and so the answer in this example is 5.
+; Given the initial block counts in your puzzle input, how many redistribution cycles must be completed before a configuration is produced that has been seen before?
+
+(ns advent-of-code.day6
+  (gen-class))
+(use 'advent-of-code.utils)
+
+(def input "0 2 7 0")
+(def pinput (parse input))
+
+(defn existing-configuration? [conf all-confs]
+  (contains? all-confs conf))
+
+(defn next-idx [curr-idx coll]
+  (mod (inc curr-idx) (count coll)))
+
+(defn distribute [start-idx val coll]
+  (loop [idx start-idx
+         v val
+         c coll]
+    (if (= v 0)
+        c
+        (recur (next-idx idx c) (dec v) (assoc c idx (inc (get c idx)))))))
+
+(defn step [coll]
+  (let [max-val (apply max coll)
+        max-idx (.indexOf coll max-val)]
+    (distribute (next-idx max-idx coll) max-val (assoc coll max-idx 0))))
+
+(defn find-config [input]
+  (loop [count 0
+         coll (parse input)
+         confs #{}]
+   (if (existing-configuration? coll confs)
+       count
+       (recur (inc count) (step coll) (conj confs coll)))))
+
+(find-config input)
+(find-config (slurp "src/advent_of_code/day6.in"))
+
+;  ---- PART TWO ----
+; Out of curiosity, the debugger would also like to know the size of the loop: starting from a state that has already been seen, how many block redistribution cycles must be performed before that same state is seen again?
+; In the example above, 2 4 1 2 is seen again after four cycles, and so the answer in that example would be 4.
+; How many cycles are in the infinite loop that arises from the configuration in your puzzle input?
+
+(defn find-config2 [input]
+  (loop [count 0
+         coll (parse input)
+         confs {}]
+   (if (existing-configuration? coll confs)
+       (- count (get confs coll))
+       (recur (inc count) (step coll) (assoc confs coll count)))))
+
+(find-config2 input)
+(find-config2 (slurp "src/advent_of_code/day6.in"))
