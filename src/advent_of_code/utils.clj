5ca63e9e214b997a669454b3382a305052bd4254
Pavel Janecka <pavel.janecka@eli-beams.eu>
2017-12-07 17:19:12 +0100

    Utils refactor

--- src/advent_of_code/utils.clj
+++ src/advent_of_code/utils.clj
@@ -1,14 +1,21 @@
 (ns advent-of-code.utils
   (:gen-class))
 
+(defn to-digit [s]
+  (Integer/parseInt s))
+
 (defn to-digits [row]
-  (map #(Integer/parseInt %)
+  (map #(to-digit %)
     row))
+
+(defn to-lines [s]
+  (->>
+    (clojure.string/split-lines s)
+    (map #(clojure.string/trim %))))
 
 (defn parse [s]
   (->>
-    (clojure.string/split-lines s)
-    (map #(clojure.string/trim %))
+    (to-lines)
     (map #(clojure.string/split % #"\s"))
     (map #(to-digits %))
     (flatten)
